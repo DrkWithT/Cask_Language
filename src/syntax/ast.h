8ac6b608c50513dbc44da2b00904c1830a18d60b
drkwitht <derektan3510@gmail.com>
2023-12-27 22:16:52 -0800

    Corrected AST decls by grammar rules.

--- src/syntax/ast.h
+++ src/syntax/ast.h
@@ -13,14 +13,16 @@ typedef enum cask_datatype_e
     CASK_DATATYPE_NIL,
     CASK_DATATYPE_INTEGER,
     CASK_DATATYPE_FLOAT,
-    CASK_DATATYPE_STRING
+    CASK_DATATYPE_STRING,
+    CASK_DATATYPE_UNKNOWN
 } DataType;
 
 typedef enum cask_comptype_e
 {
     CASK_COMPTYPE_SINGLE,
     CASK_COMPTYPE_AGGREGATE,
-    CASK_COMPTYPE_ARRAY
+    CASK_COMPTYPE_ARRAY,
+    CASK_COMPTYPE_UNKNOWN
 } CompositeType;
 
 typedef enum cask_expr_type_e
@@ -43,6 +45,7 @@ typedef enum cask_expr_type_e
 typedef enum cask_stmt_type_e
 {
     CASK_STMT_PRIMITIVE_DECL,
+    CASK_STMT_FIELD_DECL,
     CASK_STMT_AGGREGATE_DECL,
     CASK_STMT_FUNCTION_DECL,
     CASK_STMT_PARAMETER_DECL,
@@ -91,14 +94,14 @@ typedef struct cask_expr_t
 
         struct
         {
-            char *value;
+            char *name;
         } identifier;
 
         struct
         {
             struct cask_expr_t *target;
             struct cask_expr_t *key;
-            bool uses_member;
+            bool has_aggr;
         } access;
 
         struct
@@ -142,18 +145,32 @@ typedef struct cask_expr_t
 } Expression;
 
 void expression_init_special_ltrl(Expression *expr, bool is_nil, bool bool_flag);
+
 void expression_init_integer_ltrl(Expression *expr, int32_t value);
+
 void expression_init_realnum_ltrl(Expression *expr, float value);
+
 void expression_init_array_ltrl(Expression *expr);
+
 void expression_init_aggr_ltrl(Expression *expr);
+
 void expression_init_identifier_ltrl(Expression *expr, char *name);
+
+void expression_init_access(Expression *expr, Expression *target, Expression *key, bool has_aggr);
+
 void expression_init_term(Expression *expr, Expression *left, Expression *right, char op);
+
 void expression_init_factor(Expression *expr, Expression *left, Expression *right, char op);
+
 void expression_init_comparison(Expression *expr, Expression *left, Expression *right, char op);
+
 void expression_init_equality(Expression *expr, Expression *left, Expression *right, char op);
+
 void expression_init_conditional(Expression *expr, Expression *left, Expression *right, char op);
+
 bool expression_append_child(Expression *expr, ExpressionType type, Expression *child);
-void expression_dispose(Expression *expr, ExpressionType type);
+
+void expression_dispose(Expression *expr);
 
 typedef struct cask_stmt_t
 {
@@ -163,8 +180,16 @@ typedef struct cask_stmt_t
         {
             char *name;
             Expression *value;
-            uint16_t type_mask; // CompositeType : uint8_t, DataType : uint8_t
+
+            // `CompositeType : uint8_t, DataType : uint8_t`
+            uint16_t type_mask;
         } prim_decl;
+
+        struct
+        {
+            char *name;
+            uint16_t type_mask;
+        } field_decl;
 
         struct
         {
@@ -176,7 +201,7 @@ typedef struct cask_stmt_t
         {
             char *name;
             Vector params;
-            Vector stmt_body;
+            struct cask_stmt_t *block;
         } func_decl;
         
         struct
@@ -212,14 +237,30 @@ typedef struct cask_stmt_t
 } Statement;
 
 void statement_init_prim_decl(Statement *stmt, char *name, Expression *value, CompositeType high_type, DataType low_type);
+
+void statement_init_field_decl(Statement *stmt, char *name, CompositeType high_type, DataType lower_type);
+
 void statement_init_aggr_decl(Statement *stmt, char *name);
-void statement_init_func_decl(Statement *stmt, char *name);
+
+void statement_init_func_decl(Statement *stmt, char *name, Statement *block);
+
 void statement_init_param_decl(Statement *stmt, char *name, CompositeType high_type, DataType low_type);
-void statement_init_while_ctrl(Statement *stmt, Expression *conditional);
-void statement_init_if_ctrl(Statement *stmt, Expression *conditional);
-void statement_init_else_ctrl(Statement *stmt);
-bool statement_append_child(Statement *stmt, StatementType type, Statement *child);
-void statement_dispose(Statement *stmt, StatementType type);
+
+void statement_init_while_ctrl(Statement *stmt, Expression *conditional, Statement *block);
+
+void statement_init_if_ctrl(Statement *stmt, Expression *conditional, Statement *block);
+
+void statement_init_else_ctrl(Statement *stmt, Statement *block);
+
+void statement_init_block(Statement *stmt);
+
+bool statement_append_child(Statement *stmt, Statement *child);
+
+uint8_t statement_decode_comptype(const Statement *stmt);
+
+uint8_t statement_decode_datatype(const Statement *stmt);
+
+void statement_dispose(Statement *stmt);
 
 typedef struct cask_program_unit_t
 {
@@ -228,8 +269,11 @@ typedef struct cask_program_unit_t
 } ProgramUnit;
 
 void program_unit_init(ProgramUnit *prog_unit);
+
 const char *program_unit_view_name(const ProgramUnit *punit);
+
 bool program_unit_append(ProgramUnit *prog_unit, Statement *stmt_ptr);
+
 void program_unit_dispose(ProgramUnit *prog_unit);
 
 #endif
